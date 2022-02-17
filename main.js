428c5ff42a1e1402e966b635d083ef9e60b39959
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-02-17 01:08:22 -0500

    Hide/Unhide cookpot image and text

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var inputMainDish = document.querySelector('#Main-Dish');
 var inputDessert = document.querySelector('#Dessert');
 var inputEntireMeal = document.querySelector('#Entire-Meal');
 var letsCook = document.querySelector('.lets-cook-button');
-
+var cookpot = document.querySelector('.cook-pot')
+var shouldMake = document.querySelector('.should-make');
+var mealOutput = document.querySelector('.list-meal')
 var buildMeal = {};
 
 inputSide.addEventListener('click', logSide);
@@ -12,6 +14,7 @@ inputMainDish.addEventListener('click', logMainDish);
 inputDessert.addEventListener('click', logDessert);
 inputEntireMeal.addEventListener('click', logEntireMeal);
 letsCook.addEventListener('click', printMeal)
+
 
 function logSide(){
   buildMeal.side = sides[randomIndex(sides)];
@@ -27,8 +30,15 @@ function logDessert(){
 }
 function logEntireMeal(){
 }
-
 function printMeal (){
+  cookpot.classList.add('hidden');
+  shouldMake.classList.remove('hidden');
+  mealOutput.classList.remove('hidden');
+
+
+  function printDessert(){
+    console.log(desserts[randomIndex(desserts)])
+  }
 }
 
 
